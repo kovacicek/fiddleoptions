cf3c709123be40a18423cb78feb638b2c5cb30a1
Milan <milan.k@startupcodeworks.com>
2018-05-17 00:56:54 +0200

    version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "swagger-client"
-VERSION = "1.0.0"
+VERSION = "1.0.2"
 # To install the library, run the following
 #
 # python setup.py install
@@ -26,11 +26,11 @@ REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
 
 setup(
     name='FiddleOptions',
-    version='1.0.0',
+    version='1.0.2',
     description="Fiddle Options Platform",
     author_email="kovacicek@hotmail.com",
     url="https://github.com/kovacicek/fiddleoptions",
-    download_url='https://github.com/kovacicek/fiddleoptions/archive/1.0.0.tar.gz',
+    # download_url='https://github.com/kovacicek/fiddleoptions/archive/1.0.1.tar.gz',
     keywords=["Swagger", "Fiddle Options Platform"],
     install_requires=REQUIRES,
     packages=find_packages(),
